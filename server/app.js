2806631d8521fd9d2618350ad5449ef1f0a13d26
ikohyppa <iko.hyppanen@gmail.com>
2021-02-24 19:25:13 +0200

    express.static

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const loginRouter = require('./controllers/login');
 const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
+const path = require('path');
 
 const app = express();
 
@@ -33,9 +34,18 @@ app.use(express.json());
 app.use(middleware.morganLogger);
 
 app.use(middleware.tokenExtractor);
+
+app.get('/api/health', (req, res) => {
+  res.send('ok');
+});
+app.get('/api/version', (req, res) => {
+  res.send('1'); // change this string to ensure a new version deployed
+});
+
 app.use('/api/blogs', blogsRouter);
 app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
+app.use('/', express.static(path.join(__dirname, '/../client/build')));
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
