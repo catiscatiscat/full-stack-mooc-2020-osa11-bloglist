b9f735e85aa6947c968f4ce72b750743c445468c
ikohyppa <iko.hyppanen@gmail.com>
2021-02-24 19:19:23 +0200

    build

---
+++ client/build/precache-manifest.2e41ed9e845342810269fde08bb1cc4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbfb28150039a07a180d2ad87f69ce61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b079e97698d3d1ad77b2",
+    "url": "/static/css/main.a9b435a4.chunk.css"
+  },
+  {
+    "revision": "5a3589a758740ff4030b",
+    "url": "/static/js/2.d18f0b6b.chunk.js"
+  },
+  {
+    "revision": "e5067c2807f8c6eeca3663552efd09fb",
+    "url": "/static/js/2.d18f0b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b079e97698d3d1ad77b2",
+    "url": "/static/js/main.c88dc78b.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  }
+]);
